8479e75408a9ef085e0b7a3eac22510bb61128f7
= <=>
2022-05-17 18:31:22 -0300

    console.log removed

--- src/services/JsonVerifier.js
+++ src/services/JsonVerifier.js
@@ -10,8 +10,6 @@ class JsonVerifier {
   };
   headers = () => {
     const [Title, Date] = Object.keys(this.json[0]);
-    console.log(Title);
-    console.log(Date);
     if (!Title || !Date) {
       this.errors.push("expected headers are not included");
     } else console.log("headers are included");
